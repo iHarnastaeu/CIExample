6a2c121acfd3afb2332126fb8dad91c26c025add
Ivan Gornostaev <ivan.harnastaeu@schibsted.com>
2019-06-17 10:59:57 +0300

    Initial Commit

---
+++ CITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CITest
+//
+//  Created by Admin on 6/17/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
